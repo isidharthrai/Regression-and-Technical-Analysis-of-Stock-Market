6e57524a93d9cc5d8b89d2b2355145e4915d716a
Sidharth Rai <30746317+SidharthRai@users.noreply.github.com>
2019-03-06 14:25:54 +0530

    Tested Results both accurate and false.

---
+++ python script/Algorithm - 0.46 HDFC.py
@@ -0,0 +1,103 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_list_like
+pd.core.common.is_list_like = pd.api.types.is_list_like
+import pandas_datareader as data
+from sklearn import mixture as mix
+import seaborn as sns
+import matplotlib.pyplot as plt
+import talib as ta
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+end = dt.date.today()
+start = dt.datetime(end.year-5,end.month,end.day)
+df = pd.DataFrame(data.DataReader('BSE/BOM500180', 'quandl', start=start, end=end ))  #HDFC_BANK
+df.to_dense().to_csv("HDFC_df1.csv", index = False, sep=',', encoding='utf-8')
+#df.to_csv("HDFC_df.csv", sep='\t')
+
+print(df.head())
+
+df = df[['Open', 'High', 'Low', 'Close']]
+df.describe()
+
+#Spliting the data 80% for testing and 20% for training
+n = 10
+t = 0.8
+split = int(t*len(df))
+print(split)
+
+print(df['High'][:split])
+print(df['High'].shift(1))
+#I created various technical indicators such as, RSI, SMA, ADX, Correlation, 
+#Parabolic SAR, and the Return of the past 1- day on an Open to Open basis.
+
+df['High'] = df['High'].shift(1)
+df['Low'] = df['Low'].shift(1)
+df['Close'] = df['Close'].shift(1)
+df['RSI']= ta.RSI(np.array(df['Close']), timeperiod=n)
+df['SMA']= df['Close'].rolling(window=n).mean()
+df['Corr']= df['SMA'].rolling(window=n).corr(df['Close'])
+df['SAR']= ta.SAR(np.array(df['High']),np.array(df['Low']),0.2,0.2)
+df['ADX']= ta.ADX(np.array(df['High']),np.array(df['Low']),np.array(df['Close']), timeperiod=n)
+df['Return']= np.log(df['Open']/df['Open'].shift(1))
+
+df = df.dropna()
+print(df.head())
+
+
+ss = StandardScaler()
+unsup = mix.GaussianMixture(n_components=4,covariance_type='spherical', n_init=100, random_state=42)
+#df = df.drop(['High','Low','Close'], axis=1)
+unsup.fit(np.reshape(ss.fit_transform(df[:split]),(-1, df.shape[1])))
+regime=unsup.predict(np.reshape(ss.fit_transform(df[split:]),(-1, df.shape[1])))
+Regimes= pd.DataFrame(regime, columns=['Regime'],index=df[split:].index).join(df[split:], how='inner').assign(market_cu_return=df[split:].Return.cumsum()).reset_index(drop=False).rename(columns={'index':'Date'})
+
+orders=[0,1,2,3]
+fig = sns.FacetGrid(data=Regimes, hue='Regime', hue_order=orders, aspect=2, size=5, height= 10)
+fig.map(plt.scatter, 'Date', 'market_cu_return', s=4).add_legend()
+plt.show()
+
+for i in orders:
+    print("Mean for regime %i:"%i,unsup.means_[i][0])
+    print('Co-Variance for regime %i:'%i,(unsup.covariances_[i]))
+
+ss1 = StandardScaler()
+columns = Regimes.columns.drop(['Regime','Date'])
+Regimes[columns] = ss1.fit_transform(Regimes[columns])
+Regimes['Signal'] = 0
+Regimes.loc[Regimes['Return']>0, 'Signal'] = 1
+Regimes.loc[Regimes['Return']<0, 'Signal'] = -1
+
+cls = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',max_iter=-1,probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False)
+split2 = int(.8*len(Regimes))
+
+X = Regimes.drop(['Signal','Return', 'market_cu_return', 'Date'], axis=1)
+y = Regimes['Signal']
+cls.fit(X[:split2],y[:split2])
+
+print("Score: ", cls.score(X[:split2],y[:split2]))
+print(len(X))
+p_data = len(X)-split2
+print(p_data)
+
+df['Pred_Signal']=0
+df.iloc[-p_data:,df.columns.get_loc('Pred_Signal')] = cls.predict(X[split2:])
+print(df['Pred_Signal'][-p_data:].head())
+df['str_ret'] = df['Pred_Signal']*df['Return'].shift(-1)
+
+df['strategy_cu_return']=0
+df['market_cu_return']=0
+df.iloc[-p_data:,df.columns.get_loc('strategy_cu_return')] = np.nancumsum(df['str_ret'][-p_data:])
+df.iloc[-p_data:,df.columns.get_loc('market_cu_return')] = np.nancumsum(df['Return'][-p_data:])
+Sharpe = (df['strategy_cu_return'][-1]-df['market_cu_return'][-1])/np.nanstd(df['strategy_cu_return'][-p_data:])
+df.dropna()
+
+
+fig= plt.subplots(figsize = (10,10))
+plt.plot(df['strategy_cu_return'][-p_data:], color='g', label='Strategy Returns')
+plt.plot(df['market_cu_return'][-p_data:], color='r', label='Market Returns')
+plt.figtext(0.5,0.9, s='Sharpe ratio: %.2f'%Sharpe)
+plt.legend(loc='best')
+plt.show()
